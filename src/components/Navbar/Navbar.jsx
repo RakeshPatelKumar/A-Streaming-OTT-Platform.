6fcd87574d35bd774a64b45a5f144f9477081915
Rakesh Kumar <154679938+RakeshPatelKumar@users.noreply.github.com>
2025-07-13 20:58:31 +0530

    navbaar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,17 +13,14 @@ const Navbar = () => {
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY >= 80) {
-        navRef.current.classList.add('navbar-dark'); // Add class on scroll
+        navRef.current.classList.add('navbar-dark');
       } else {
-        navRef.current.classList.remove('navbar-dark'); // Remove class
+        navRef.current.classList.remove('navbar-dark');
       }
     };
 
     window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll); // Cleanup on unmount
-    };
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
@@ -31,23 +28,25 @@ const Navbar = () => {
       <div className="navbar-left">
         <img src={logo} alt="Logo" />
         <ul>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>New & Popular</li>
-          <li>My List</li>
-          <li>Browse by Language</li>
+          <li><a href="https://example.com/home">Home</a></li>
+          <li><a href="https://example.com/tv-shows">TV Shows</a></li>
+          <li><a href="https://example.com/movies">Movies</a></li>
+          <li><a href="https://example.com/new-popular">New & Popular</a></li>
+          <li><a href="https://example.com/my-list">My List</a></li>
+          <li><a href="https://example.com/language">Browse by Language</a></li>
         </ul>
       </div>
+
       <div className="navbar-right">
         <img src={search_icon} alt="search" className="icons" />
         <p>Children</p>
-        <img src={bell_icon1} alt="search" className="icons" />
+        <img src={bell_icon1} alt="bell" className="icons" />
+
         <div className="navbar-profile">
-          <img src={profile_img} alt="search" className="profile" />
-          <img src={dropdownicon} alt="" className="icons" />
+          <img src={profile_img} alt="profile" className="profile" />
+          <img src={dropdownicon} alt="dropdown" className="icons" />
           <div className="dropdown">
-            <p onClick={()=>{logout()}}>Sign out of Netflix</p>
+            <p onClick={() => logout()}>Sign out of Netflix</p>
           </div>
         </div>
       </div>
