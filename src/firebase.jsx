31e9188714af6443faec95ad388b7a14afe577c6
Rakesh Kumar <154679938+RakeshPatelKumar@users.noreply.github.com>
2025-07-13 20:01:27 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -12,13 +12,14 @@ import {
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-  apiKey: "<your api key>",
-  authDomain: "netflix-clone-a5a55.firebaseapp.com",
-  projectId: "netflix-clone-a5a55",
-  storageBucket: "netflix-clone-a5a55.firebasestorage.app",
-  messagingSenderId: "562120484015",
-  appId: "1:562120484015:web:6dc3a8949a86911bfca05a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
 
 
 const app = initializeApp(firebaseConfig);
